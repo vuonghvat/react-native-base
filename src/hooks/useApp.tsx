41918d95d86b7784d7801d8031afa8d7b355848a
vuonghvat <vuonghvat@gmail.com>
2022-05-17 15:15:14 +0700

    splash screen fixed

--- src/hooks/useApp.tsx
+++ src/hooks/useApp.tsx
@@ -3,9 +3,14 @@ import UserApi from "../networking/apis/User";
 import {shallowEqual, useSelector, useDispatch} from "react-redux";
 import {RootState} from "@app/app-reduxs/store";
 import LoginActions from "@app/app-reduxs/login/actions";
+import SplashScreen from "react-native-splash-screen";
 export default () => {
   const [token, setToken] = useState("");
   const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return {
     isLoading,
