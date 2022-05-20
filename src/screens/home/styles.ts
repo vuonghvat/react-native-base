b7f7077da16b01216bd55e02ba85340ff107448f
vuonghvat <vuonghvat@gmail.com>
2022-05-20 09:22:54 +0700

    add auth flow

--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -4,5 +4,10 @@ export default StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "blue"
-    }
+    },
+    button:{
+        height:50,
+        backgroundColor:"red",
+        alignSelf:"center"
+      }
 })
